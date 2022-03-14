787e0654775bdb9ead48d6762f49b691a25bc9af
Samuel-Jangbro-NTIJohanneberg <94970170+Samuel-Jangbro-NTIJohanneberg@users.noreply.github.com>
2022-03-14 08:53:52 +0100

    uhh

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-const nav = document.querySelector('.nackLinks')
+const nav = document.querySelector('.navLinks')
 const burger = document.querySelector('.burger')
-
 
 function toggleMeny() {
    nav.classList.toggle('navActive')
-   burger.classList.toggle()
+   burger.classList.toggle('toggle')
 }
 burger.addEventListener('click', toggleMeny)
